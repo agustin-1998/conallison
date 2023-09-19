bf7a4f2e6aa8e745b23660b65b461a75ed96ce27
Agus <agus.rodriguez777@gmail.com>
2023-09-19 09:55:35 -0300

    add

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -9,10 +9,10 @@ export const Footer = () => {
                 <ul className='max-w-5xl mx-auto flex flex-col md:flex-row md:items-center justify-between md:space-x-5 space-y-5 md:space-y-0'>
                     <li>
                         <Link href={'http://conallison.com/'} target='_blank'>
-                            <Image src={'/assets/conallison.jpg'} width={200} height={50} alt='' />
+                            <Image src={'/assets/conallison.jpg'} width={200} height={50} alt='conallison-logo' />
                         </Link>
                     </li>
-                    <div className='flex flex-col md:flex-row md:space-x-5 space-y-5 md:space-y-0 text-xl'>
+                    <div className='flex flex-col md:flex-row md:space-x-5 space-y-5 md:space-y-0 text-lg'>
                         <li>
                             <span className='text-black'>E-mail: </span>
                             <Link href={'mailto:mailto:conallison@conallison.com'} className='text-black underline hover:text-blue-700'>
@@ -22,7 +22,7 @@ export const Footer = () => {
                         <li>
                             <span className='text-black'>Tel: </span>
                             <Link href={'tel:+541122974908'} className='text-black underline hover:text-blue-700'>
-                                +54 01122974908
+                                +54 91122974908
                             </Link>
                         </li>
                     </div>
