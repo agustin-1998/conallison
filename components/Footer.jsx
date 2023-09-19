6f8511e58f0ac8e0ff73cfe3111bae3a538bb60c
Agus <agus.rodriguez777@gmail.com>
2023-09-19 10:19:08 -0300

    update

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -12,16 +12,16 @@ export const Footer = () => {
                             <Image src={'/assets/conallison.jpg'} width={200} height={50} alt='conallison-logo' />
                         </Link>
                     </li>
-                    <div className='flex flex-col md:flex-row md:space-x-5 space-y-5 md:space-y-0 text-lg'>
+                    <div className='flex flex-col lg:flex-row md:space-x-5 space-y-5 md:space-y-0 text-lg'>
                         <li>
                             <span className='text-black'>E-mail: </span>
                             <Link href={'mailto:mailto:conallison@conallison.com'} className='text-black underline hover:text-blue-700'>
                                 conallison@conallison.com
                             </Link>
                         </li>
-                        <li>
-                            <span className='text-black'>Tel: </span>
-                            <Link href={'tel:+541122974908'} className='text-black underline hover:text-blue-700'>
+                        <li className='flex space-x-3'>
+                            <span className='text-black'><Image src={'/assets/wpp-icon.png'} width={30} height={30} loading='lazy' alt='whatsapp-icon' /></span>
+                            <Link href={'https://wa.me/5491122974908'} className='text-black underline hover:text-blue-700'>
                                 +54 91122974908
                             </Link>
                         </li>
