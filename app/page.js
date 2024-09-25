caf0e25e3fee20974130c70951fdfff57247a824
Agus <agus.rodriguez777@gmail.com>
2024-09-25 12:37:14 -0300

    fix

--- app/page.js
+++ app/page.js
@@ -1,12 +1,10 @@
 import Body from '@/components/Body'
 import { Footer } from '@/components/Footer'
-// import Header from '@/components/Header'
 
 export default function Home() {
   return (
     <>
       <div className="flex flex-col min-h-screen">
-        {/* <Header /> */}
         <main className="flex-1">
           <Body />
         </main>
