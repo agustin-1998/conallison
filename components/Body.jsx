8243eed94bbc787a22473c11ca3572ce1b849de4
Agus <agus.rodriguez777@gmail.com>
2024-09-25 12:31:15 -0300

    new images and better structure

--- components/Body.jsx
+++ components/Body.jsx
@@ -4,30 +4,38 @@ import React from 'react'
 const Body = () => {
     return (
         <>
-            <section className='max-w-5xl mx-auto mt-6 sm:mt-9 mb-[27px]'>
-                <div className='flex flex-col justify-center items-center space-y-5 px-3 lg:px-0'>
-                    <figure>
-                        <Image 
-                            src={'/assets/poster-1.webp'} 
-                            width={700} 
-                            height={700}  
-                            alt='conallison-logo' 
-                            loading='lazy' 
-                            quality={20} 
-                        />
-                    </figure>
-                    <figure>
-                        <Image 
-                            src={'/assets/poster-2.webp'} 
-                            width={700} 
-                            height={700} 
-                            alt='conallison-logo' 
-                            loading='lazy' 
-                            quality={20}
-                        />
-                    </figure>
-                </div>
-            </section>
+            <div className='flex flex-col justify-center items-center flex-1 space-y-5 px-3 lg:px-0 mx-auto mt-6 sm:mt-9 md:mb-9'>
+                <figure>
+                    <Image 
+                        src={'/assets/muge-2.jpg'} 
+                        width={700} 
+                        height={700}  
+                        alt='conallison-logo-1' 
+                        loading='lazy' 
+                        quality={20} 
+                    />
+                </figure>
+                <figure>
+                    <Image 
+                        src={'/assets/muge-1.jpg'} 
+                        width={700} 
+                        height={700} 
+                        alt='conallison-logo-2' 
+                        loading='lazy' 
+                        quality={20}
+                    />
+                </figure>
+                <figure>
+                    <Image 
+                        src={'/assets/muge-3.jpg'} 
+                        width={700} 
+                        height={700} 
+                        alt='conallison-logo-3' 
+                        loading='lazy' 
+                        quality={20}
+                    />
+                </figure>
+            </div>
         </>
     )
 }
