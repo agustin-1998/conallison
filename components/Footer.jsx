be35c31a780d77d763fff220e8fb6f1706a28dd9
agustin-1998 <agus.rodriguez777@gmail.com>
2026-05-08 15:34:28 -0300

    Remove WeChat contact link from Footer component

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -25,13 +25,6 @@ export const Footer = () => {
                         </li>
                         <li className='flex items-center space-x-2 hover:border-b hover:border-b-black'>
                             <div className='flex items-center'>
-                                <Link href={'weixin://addfriend/wxid_0vs34w3shqen22'} className='flex items-center space-x-1'>
-                                    <Image src={'/assets/wechat-icon.svg'} width={30} height={30} loading='lazy' alt='wechat-icon' />
-                                </Link>
-                            </div>
-                        </li>
-                        <li className='flex items-center space-x-2 hover:border-b hover:border-b-black'>
-                            <div className='flex items-center'>
                                 <Link href={'https://wa.me/5491123310789'} target='_blank' className='flex items-center space-x-1'>
                                     <Image src={'/assets/wpp-icon.png'} width={25} height={25} loading='lazy' alt='whatsapp-sales' />
                                     <span className='text-xs text-gray-600'>Export Sales</span>
